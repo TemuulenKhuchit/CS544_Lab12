eb297ea0e180b42c8af443a7f646caaa21c0bedb
Temuulen <temuulenkhuchit01@gmail.com>
2024-12-13 21:05:41 -0600

    Initial commit

---
+++ src/test/java/edu/miu/cs544/temuulen/lab12/Lab12ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.temuulen.lab12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
